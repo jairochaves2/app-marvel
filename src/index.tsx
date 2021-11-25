5ad27a885794820ba5973875ce6449d269c45478
Jairo Chaves <jairochaves2@gmail.com>
2021-11-25 14:45:22 -0300

    feat: add SelectionProvider

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import SelectionProvider from "./providers/ComicSelectionProvider";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <SelectionProvider>
+      <App />
+    </SelectionProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
