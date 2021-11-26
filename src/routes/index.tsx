469ab9fcc1f9aaffad3a2724f85ff4c4fb7a8d13
Jairo Chaves <jairochaves2@gmail.com>
2021-11-26 17:08:02 -0300

    feat: add pagina 404

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import App from "../App";
+import Page404 from "../views/pages/404";
+// import App from "../App";
 import Home from "../views/pages/Home";
 
 const RouteProvider: React.FC = () => {
@@ -8,7 +9,7 @@ const RouteProvider: React.FC = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/old" element={<App />} />
+        <Route path="*" element={<Page404 />} />
       </Routes>
     </BrowserRouter>
   );
